eb65f10237e645035f3031f77ffcddc2f3cab900
jyothika-79 <89721714+jyothika-79@users.noreply.github.com>
2025-09-03 16:04:13 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,3 +1,4 @@
+yum install wget -y
 yum install java-17-amazon-corretto -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
 tar -zxvf apache-tomcat-9.0.108.tar.gz
